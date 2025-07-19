a3fdb8fa26deeb4e94eb59b60ef804014b005cb8
Bushwack21 <roberthollander206@gmail.com>
2025-07-19 19:27:59 -0400

    Added github auth + callback

--- backend-proper/server.py
+++ backend-proper/server.py
@@ -3,21 +3,21 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 from routes.api_routes import api_blueprint
+from flask_cors import CORS
   # Make sure 'routes' is a Blueprint instance in routes.py
 
 
 
 load_dotenv('info.env')
-
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])
 app.register_blueprint(api_blueprint)   # 'routes' must be a Blueprint, not a module or function
 
 # MongoDB Setup
-mongo_uri = fmongo_uri = "mongodb+srv://ht6:1234@ht6.wpk555k.mongodb.net/ht6?retryWrites=true&w=majority&appName=HT6"
-
+mongo_uri = f"mongodb+srv://ht6:1234@ht6.wpk555k.mongodb.net/ht6?retryWrites=true&w=majority&appName=HT6"
 client = MongoClient(mongo_uri)
 db = client.get_database()
 client.server_info()  # This will raise an exception if the connection fails
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(port=5173, debug=True)
